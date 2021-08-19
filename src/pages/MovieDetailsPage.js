98fd3c6d77ab8b461ed150bc05c48bf15f3543cb
Halyna Sinko <galinasinko14@gmail.com>
2021-08-20 00:40:53 +0300

    Create MovieDetailsPage.js

---
+++ src/pages/MovieDetailsPage.js
@@ -0,0 +1,13 @@
+// import { useState, useEffect } from "react";
+
+export default function MovieDetailsPage() {
+  //   const [movie, setMovie] = useState(null);
+
+  //   useEffect(() => {});
+  return (
+    <>
+      <h2>film</h2>
+      <p>77777</p>
+    </>
+  );
+}
