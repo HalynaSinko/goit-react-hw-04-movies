7b85c57528d5c0f69867565c374265543c5627ce
Halyna Sinko <galinasinko14@gmail.com>
2021-08-19 21:17:11 +0300

    add Switch and Route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { Switch, Route } from "react-router-dom";
+
+import Container from "./components/Container";
+import AppBar from "./components/AppBar";
+import HomePage from "./pages/HomePage";
+import MoviesPage from "./pages/MoviesPage";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <Container>
+      <AppBar />
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route path="/movies">
+          <MoviesPage />
+        </Route>
+      </Switch>
+    </Container>
   );
 }
 
