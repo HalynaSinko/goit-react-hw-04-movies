1bfa7008ef899e372d384387267cd7b3fe4b58cc
Halyna Sinko <galinasinko14@gmail.com>
2021-08-20 00:42:38 +0300

    add loading trending movies

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,32 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
+import * as moviesApi from "../services/moviesApi";
 import PageHeading from "../components/PageHeading";
 
 export default function HomePage() {
   const [trendingMovies, setTrendingMovies] = useState(null);
 
-  useEffect(() => {}, []);
-  return <PageHeading text="Trending today" />;
+  useEffect(() => {
+    moviesApi
+      .fetchTrendingMovies()
+      .then(({ results }) => setTrendingMovies(results));
+  }, []);
+
+  return (
+    <>
+      <PageHeading text="Trending today" />
+      {trendingMovies && (
+        <ul>
+          {trendingMovies.map((movie) => (
+            <li key={movie.id}>
+              <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
+
+// console.log(results);
