22ec938fdf9b7e305247de68c02d4f7a4209a0b3
Halyna Sinko <galinasinko14@gmail.com>
2021-08-21 12:51:30 +0300

    apdate MovieDetailsPage

--- src/pages/MovieDetailsPage.js
+++ src/pages/MovieDetailsPage.js
@@ -1,13 +1,37 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import * as moviesApi from "../services/moviesApi";
+import PageHeading from "../components/PageHeading";
 
 export default function MovieDetailsPage() {
-  //   const [movie, setMovie] = useState(null);
+  const [movie, setMovie] = useState(null);
 
-  //   useEffect(() => {});
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    moviesApi.fetchMovieDetails(movieId).then(setMovie);
+  }, [movieId]);
+
+  const genres = movie ? movie.genres.map((item) => item.name).join(" ") : null;
+
   return (
-    <>
-      <h2>film</h2>
-      <p>77777</p>
-    </>
+    movie && (
+      <div>
+        <img
+          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+          alt={movie.title}
+        />
+        <div>
+          <PageHeading text={movie.title} />
+          <p>User scope:{movie.vote_average * 10}%</p>
+          <p>Overview</p>
+          <p>{movie.overview}</p>
+          <p>Genres</p>
+          <p>{genres}</p>
+        </div>
+      </div>
+    )
   );
 }
+// console.log(movie);
