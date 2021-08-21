ca0dd2194db8b2a099f45b7b9f000d11ff9ff0f3
Halyna Sinko <galinasinko14@gmail.com>
2021-08-21 12:42:55 +0300

    create SearchForm

---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+import s from "./SearchForm.module.css";
+
+export default function SearchForm({ onSubmit }) {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleChangeInput = (event) => {
+    setSearchQuery(event.target.value.toLowerCase());
+  };
+
+  const handleSubmitForm = (event) => {
+    event.preventDefault();
+
+    if (searchQuery.trim() === "") {
+      return;
+    }
+
+    onSubmit(searchQuery);
+    setSearchQuery("");
+  };
+
+  return (
+    <form className={s.searchForm} onSubmit={handleSubmitForm}>
+      <input
+        className={s.input}
+        name="searchQuery"
+        value={searchQuery}
+        type="text"
+        autoComplete="off"
+        autoFocus
+        placeholder="Search movie"
+        onChange={handleChangeInput}
+      />
+
+      <button type="submit" className={s.button}>
+        Search
+      </button>
+    </form>
+  );
+}
+
+SearchForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
