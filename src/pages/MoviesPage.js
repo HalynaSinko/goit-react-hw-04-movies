39056f2b8c2eaa1667dd5f809830992ada79997a
Halyna Sinko <galinasinko14@gmail.com>
2021-08-22 00:21:58 +0300

    Update MoviesPage.js

--- src/pages/MoviesPage.js
+++ src/pages/MoviesPage.js
@@ -1,3 +1,41 @@
+import { useState, useEffect } from "react";
+import { Link, useRouteMatch } from "react-router-dom";
+// import PropTypes from "prop-types";
+import SearchForm from "../components/SearchForm";
+import * as moviesApi from "../services/moviesApi";
+
 export default function MoviesPage() {
-  return <p> Movies Page</p>;
+  const { url } = useRouteMatch();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  // console.log(url);
+  function hendleOnSubmit(query) {
+    setSearchQuery(query);
+  }
+
+  useEffect(() => {
+    if (!searchQuery) {
+      return;
+    }
+    moviesApi.fetchMovieByQuery(searchQuery).then(({ results }) => {
+      setMovies(results);
+    });
+  }, [searchQuery]);
+
+  return (
+    <div>
+      <p> Movies Page</p>
+      <SearchForm onSubmit={hendleOnSubmit} />
+      {movies && (
+        <ul>
+          {movies.map((movie) => (
+            <li key={movie.id}>
+              <Link to={`${url}/${movie.id}`}>{movie.original_title}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
