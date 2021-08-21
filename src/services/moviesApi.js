0601690f4dc64c6b291cc12f3f16c0a8d2b2dba3
Halyna Sinko <galinasinko14@gmail.com>
2021-08-22 00:20:20 +0300

    add fetch by query,  cast, revievs.

--- src/services/moviesApi.js
+++ src/services/moviesApi.js
@@ -24,3 +24,26 @@ export function fetchMovieDetails(movieId) {
   return fetchWithErrorHendling(`${BASE_URL}
 /movie/${movieId}?api_key=${API_KEY}&language=en-US`);
 }
+
+// https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&query=<<query>>&page=1&include_adult=false
+
+export function fetchMovieByQuery(query) {
+  return fetchWithErrorHendling(`${BASE_URL}
+/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`);
+}
+
+// https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=<<api_key>>&language=en-US
+
+export function fetchActorsMovie(movieId) {
+  return fetchWithErrorHendling(
+    `${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`
+  );
+}
+
+// https://api.themoviedb.org/3/movie/{movie_id}/reviews?api_key=<<api_key>>&language=en-US&page=1
+
+export function fetchReviewsMovie(movieId) {
+  return fetchWithErrorHendling(
+    `${BASE_URL}/movie/${movieId}/reviews?api_key=${API_KEY}&language=en-US&page=1`
+  );
+}
