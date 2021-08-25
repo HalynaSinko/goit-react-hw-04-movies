5338ab03a05f2adf909247967e53824cac689576
Halyna Sinko <galinasinko14@gmail.com>
2021-08-25 03:58:00 +0300

    change architecture

---
+++ src/pages/MoviesPage/MoviesPage.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import { useRouteMatch, useLocation, useHistory } from "react-router-dom";
+import { toast } from "react-toastify";
+// import PropTypes from "prop-types";
+import SearchForm from "../../components/SearchForm";
+import * as moviesApi from "../../services/moviesApi";
+import MoviesGallery from "../../components/MoviesGallery";
+
+export default function MoviesPage() {
+  const { url } = useRouteMatch();
+  const location = useLocation();
+  const history = useHistory();
+  console.log(location);
+  const query = new URLSearchParams(location.search).get("query");
+
+  const [searchQuery, setSearchQuery] = useState(query ?? "");
+  const [movies, setMovies] = useState([]);
+
+  function hendleOnSubmit(query) {
+    setSearchQuery(query);
+    history.push({ ...location, search: `query=${query}` });
+  }
+
+  useEffect(() => {
+    if (!searchQuery) {
+      return;
+    }
+    moviesApi.fetchMovieByQuery(searchQuery).then(({ results }) => {
+      if (results.length === 0) {
+        toast.error("Results not found.");
+      }
+      setMovies(results);
+    });
+  }, [searchQuery]);
+
+  return (
+    <div>
+      <SearchForm onSubmit={hendleOnSubmit} />
+      {movies && (
+        <MoviesGallery movies={movies} url={url} location={location} />
+      )}
+    </div>
+  );
+}
